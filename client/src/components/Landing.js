9ee50b4e1f1643c21b7ad6ba2ae03c2658d0252b
Giri <gvakka@gmail.com>
2017-08-23 12:41:51 -0400

    Added billing and client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Boilerplate!</h1>
+      Good starting point.
+    </div>
+  );
+};
+
+export default Landing;

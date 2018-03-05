e03215d0d4c5d97fe0e8c70e2f5b3686cad52aa8
Giri <gvakka@gmail.com>
2018-03-05 12:02:55 -0500

    updated ui tests

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -6,33 +6,28 @@ import {
   Menu,
   Image,
   Icon,
-  Header
+  Header,
+  Container
 } from 'semantic-ui-react';
 
 class SidebarTopPush extends Component {
   render() {
     return (
       <div>
-        <Sidebar
-          as={Menu}
-          animation="push"
-          direction="top"
-          visible={true}
-          inverted
-        >
-          <Menu.Item position="right" name="home">
-            <Icon name="home" />
-            Home
+        <Menu attached="top" borderless fitted visible={true} inverted>
+          <Menu.Item active={true} position="right" name="home">
+            <Icon color="yellow" name="home" />
+            <p style={{ color: 'yellow' }}>Home</p>
           </Menu.Item>
           <Menu.Item name="gamepad">
             <Icon name="gamepad" />
             Games
           </Menu.Item>
-          <Menu.Item style={{ paddingRight: '175' }} name="camera">
+          <Menu.Item name="camera">
             <Icon name="camera" />
             Channels
           </Menu.Item>
-        </Sidebar>
+        </Menu>
       </div>
     );
   }

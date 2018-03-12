54ca6f99175eb1b1c5b0c56af2e56190b2e01538
Giri <gvakka@gmail.com>
2018-03-12 04:35:53 -0400

    added passport strategies

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 const User = mongoose.model('users');
@@ -58,6 +60,80 @@ passport.use(
       // });
 
       done(null, user);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookClientID,
+      clientSecret: keys.facebookClientSecret,
+      callbackURL: '/auth/facebook/callback',
+      profileFields: ['id', 'displayName', 'email', 'photos'],
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      //console.log('access token ', accessToken);
+      //console.log('refresh token ', refreshToken);
+      //console.log('profile:', profile);
+
+      const existingUser = await User.findOne({ facebookId: profile.id });
+
+      if (existingUser) {
+        //console.log(existingUser);
+        return done(null, existingUser);
+      }
+
+      const user = await new User({
+        facebookId: profile.id,
+        email: profile.emails[0].value,
+        name: profile.displayName,
+        pic: profile.photos[0].value
+      }).save();
+      //console.log(user);
+      const algoliaObjectID = user.id;
+      user.objectID = user.id;
+      user.save();
+      done(null, user);
+    } //console.log(user);
+  )
+);
+
+passport.use(
+  new LinkedInStrategy(
+    {
+      clientID: keys.linkedinClientID,
+      clientSecret: keys.linkedinClientSecret,
+      callbackURL: '/auth/linkedin/callback',
+      scope: ['r_emailaddress', 'r_basicprofile'],
+      state: true,
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      process.nextTick(async () => {
+        //console.log(profile.emails[0].value);
+        const existingUser = await User.findOne({ linkedinId: profile.id });
+
+        if (existingUser) {
+          //console.log(existingUser);
+          return done(null, existingUser);
+        }
+
+        const user = await new User({
+          linkedinId: profile.id,
+          email: profile.emails[0].value,
+          name: profile.displayName,
+          pic: profile.photos[0].value
+        }).save();
+        //console.log(user);
+
+        const algoliaObjectID = user.id;
+        user.objectID = user.id;
+        user.save();
+
+        return done(null, user);
+      });
     }
   )
 );
